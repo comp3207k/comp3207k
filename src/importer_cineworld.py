b18020af01579c4ebd473003dd91cb28092c9fbc
James Crickmere <james.crickmere@gmail.com>
2014-10-20 21:51:35 +0100

    Addede importers

---
+++ src/importer_cineworld.py
@@ -0,0 +1,16 @@
+""""
+importer_cineworld.py
+
+Defines the CineWorldImporter class.
+"""
+
+
+import importer
+
+
+class CineWorldImporter(importer.Importer):
+    
+    def import_data(self):
+        pass
+
+
---
+++ src/importer_imdb.py
@@ -0,0 +1,16 @@
+""""
+importer_imdb.py
+
+Defines the IMDBImporter class.
+"""
+
+
+import importer
+
+
+class IMDBImporter(importer.Importer):
+    
+    def import_data(self):
+        pass
+
+
