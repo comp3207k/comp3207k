449905f7dbe3ae70a1f4507c5d8759417fabc857
James Crickmere <james.crickmere@gmail.com>
2014-10-20 21:44:01 +0100

    Created Importer superclass

---
+++ src/importer.py
@@ -0,0 +1,21 @@
+"""
+importer.py
+
+Contains the root class for importers, who fetch data from external data
+sources and add them to the database.
+"""
+
+
+class ImporterException(Exception):
+    pass
+
+
+
+class Importer(object):
+    
+    def import_data(self):
+        """
+        Does the importing; throws ImporterException on error.
+        """
+
+        raise NotImplementedError('Method must be defined in subclass')
