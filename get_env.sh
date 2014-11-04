d5627cbcb711ba7ec523510b33159b45163335a4
James Crickmere <james.crickmere@gmail.com>
2014-11-04 14:09:28 +0000

    updated get env

--- get_env.sh
+++ get_env.sh
@@ -2,14 +2,14 @@
 
 # Downloads the GAE SDK
 
-wget -nc https://storage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.13.zip
+wget -nc https://storage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.14.zip
 
-hash=$(sha1sum google_appengine_1.9.13.zip)
-if [ "${hash}" != "05166691108caddc4d4cfdf683cfc4748df197a2  google_appengine_1.9.13.zip" ]; then
+hash=$(sha1sum google_appengine_1.9.14.zip)
+if [ "${hash}" != "5d58fc7414c17920281a661e66be5877bc30c379  google_appengine_1.9.14.zip" ]; then
     echo "Download is either corrupt, or the hash has changed!";
     exit;
 fi
 
-unzip -q google_appengine_1.9.13.zip
-rm google_appengine_1.9.13.zip
+unzip -q google_appengine_1.9.14.zip
+rm google_appengine_1.9.14.zip
 
