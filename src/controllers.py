8906946299f2073c104ab8f7d913cfa49c0fa2a5
James Crickmere <james.crickmere@gmail.com>
2014-11-28 14:36:40 +0000

    Added jinja and make login render work

--- src/controllers.py
+++ src/controllers.py
@@ -2,18 +2,43 @@
 Request handlers for non-static pages go here.
 """
 
-
+import os
 
 import webapp2
+import jinja2
 
-class IndexPage(webapp2.RequestHandler):
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+
+class RootHandler(webapp2.RequestHandler):
+    
+    def render(self, template, vars):
+        template = JINJA_ENVIRONMENT.get_template(template)
+        self.response.write(template.render(vars))
+
+
+
+class Index(RootHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello, World!')
 
+
+class Login(RootHandler):
+    def get(self):
+        self.render('index.html', {})
+
 application = webapp2.WSGIApplication([
-    ('/', IndexPage),
+    ('/', Index),
+    ('/login', Login)
 ], debug=True)
 
+
+
+
+        
 
 
