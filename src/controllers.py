6a28b0bfb14cf7edfa4ebb22f43ec1378d59be35
Popovici Petru Dragos <dragosp_25@yahoo.com>
2015-01-01 20:58:17 +0000

    Login and Sign Up work

--- src/controllers.py
+++ src/controllers.py
@@ -6,6 +6,22 @@ import os
 
 import webapp2
 import jinja2
+from handlers import *
+
+config = {
+  'webapp2_extras.auth': {
+    'user_model': 'models.User',
+    'user_attributes': ['name']
+  },
+  'webapp2_extras.sessions': {
+    'secret_key': 'YOUR_SECRET_KEY'
+  }
+}
+
+
+
+logging.getLogger().setLevel(logging.DEBUG)
+
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')),
@@ -32,11 +48,16 @@ class Login(RootHandler):
         self.render('index.html', {})
 
 application = webapp2.WSGIApplication([
-    ('/', Index),
-    ('/login', Login)
-], debug=True)
-
-
+    webapp2.Route('/', MainHandler, name='home'),
+    webapp2.Route('/signup', SignupHandler),
+    webapp2.Route('/<type:v|p>/<user_id:\d+>-<signup_token:.+>',
+      handler=VerificationHandler, name='verification'),
+    webapp2.Route('/password', SetPasswordHandler),
+    webapp2.Route('/login', LoginHandler, name='login'),
+    webapp2.Route('/logout', LogoutHandler, name='logout'),
+    webapp2.Route('/forgot', ForgotPasswordHandler, name='forgot'),
+    webapp2.Route('/authenticated', AuthenticatedHandler, name='authenticated')
+], debug=True, config=config)
 
 
         
