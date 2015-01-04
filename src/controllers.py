649b88bbce4d07a7d3010d25d328211de0f912be
James Crickmere <james.crickmere@gmail.com>
2015-01-04 17:51:43 +0000

    Search JSON works

--- src/controllers.py
+++ src/controllers.py
@@ -9,6 +9,8 @@ import jinja2
 from google.appengine.api import taskqueue
 
 from handlers import *
+import search
+import models
 
 config = {
   'webapp2_extras.auth': {
@@ -26,7 +28,7 @@ logging.getLogger().setLevel(logging.DEBUG)
 
 
 JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')),
+    loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'views')),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
@@ -39,18 +41,16 @@ class RootHandler(webapp2.RequestHandler):
 
 
 
-class Index(RootHandler):
-    def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
 
 
-class Login(RootHandler):
-    def get(self):
-        self.render('index.html', {})
+
 
 
 class Import(RootHandler):
+    """
+    Kicks off the ImportWorker.
+    """
+    
     def get(self):
         taskqueue.add(url='/import_worker', retry_options=taskqueue.TaskRetryOptions(task_retry_limit=0))
         
@@ -58,6 +58,10 @@ class Import(RootHandler):
         self.response.write('Task started (check status at http://localhost:8000/taskqueue or https://appengine.google.com/)')
 
 class ImportWorker(RootHandler):
+    """
+    A task that import CineWorld data.
+    """
+    
     def post(self):
         import importer_cineworld
         i = importer_cineworld.CineWorldImporter()
@@ -65,6 +69,53 @@ class ImportWorker(RootHandler):
         
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Done')
+
+class Index(RootHandler):
+    """
+    Kicks off the IndexWorker.
+    """
+    
+    def get(self):
+        taskqueue.add(url='/index_worker', retry_options=taskqueue.TaskRetryOptions(task_retry_limit=0))
+        
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Task started (check status at http://localhost:8000/taskqueue or https://appengine.google.com/)')
+
+class IndexWorker(RootHandler):
+    """
+    A task that adds new cinemas and films to the index.
+    """
+    
+    def post(self):
+        cinemas = models.Cinemas.query().fetch(1000)
+        
+        for c in cinemas:
+            search.import_cinema(c)
+        
+        films = models.Films.query().fetch(1000)
+        
+        for f in films:
+            search.import_film(f)
+
+
+class SearchAJAX(RootHandler):
+    """Returns JSON based on search query"""
+    
+    def get(self):
+        query = self.request.get('q')[:100].strip()
+        cinemas, films = search.query(query)
+        
+        self.response.headers['Content-Type'] = 'application/json'
+        
+        self.render('search_ajax.json', {
+            'n_cinemas': cinemas.number_found,
+            'results_cinemas': cinemas.results,
+            'n_films': films.number_found,
+            'results_films': films.results,
+        })
+        
+
+
 
 application = webapp2.WSGIApplication([
     webapp2.Route('/', MainHandler, name='home'),
@@ -78,9 +129,12 @@ application = webapp2.WSGIApplication([
     webapp2.Route('/authenticated', AuthenticatedHandler, name='authenticated'),
     webapp2.Route('/import', Import),
     webapp2.Route('/import_worker', ImportWorker),
+    webapp2.Route('/index', Index),
+    webapp2.Route('/index_worker', IndexWorker),
     webapp2.Route('/about', AboutHandler),
     webapp2.Route('/contact', ContactHandler),
     webapp2.Route('/search', SearchHandler),
+    webapp2.Route('/search_ajax', SearchAJAX),
 ], debug=True, config=config)
 
 
---
+++ src/search.py
@@ -0,0 +1,84 @@
+"""
+search.py
+
+Contains functions to import Films into the search index,
+and perform full text searches.
+Uses the Google Search API.
+"""
+
+from google.appengine.api import search 
+
+
+def import_film(film):
+    """Adds a models.Films object into the index."""
+    
+    index = get_film_index()
+    doc = index.get(generate_film_id(film))
+    
+    if doc is not None:
+        return
+    
+    document = search.Document(
+        doc_id = generate_film_id(film),
+        fields = [
+            search.TextField(name='title', value=film.title),
+            search.TextField(name='synopsis', value=film.synopsis),
+            search.AtomField(name='poster', value=film.poster),
+            search.AtomField(name='api_id', value=film.api_id),
+        ]
+    )
+    
+    index.put(document)
+
+
+def import_cinema(cinema):
+    """Adds a models.Cinemas object into the index."""
+    
+    index = get_cinema_index()
+    doc = index.get(generate_cinema_id(cinema))
+    
+    if doc is not None:
+        return
+    
+    document = search.Document(
+        doc_id = generate_cinema_id(cinema),
+        fields = [
+            search.TextField(name='name', value=cinema.name),
+            search.AtomField(name='api_id', value=cinema.api_id),
+        ]
+    )
+    
+    index.put(document)
+
+
+def query(string):
+    """
+    Searches films and cinemas and returns (cinemas, films) as
+    SearchResults objects.
+    """
+    
+    return get_cinema_index().search(string), get_film_index().search(string)
+
+
+# Methods below are private to this module
+
+def get_film_index():
+    return search.Index(name='films')
+
+def get_cinema_index():
+    return search.Index(name='cinemas')
+
+
+def generate_film_id(film):
+    """Generates a unique id for a models.Film object."""
+    
+    return film.api_id
+
+
+def generate_cinema_id(cinema):
+    """Generates a unique id for a models.Cinema object."""
+    
+    return cinema.api_id
+
+    
+
